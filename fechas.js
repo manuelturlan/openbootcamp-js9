c46638605fc209448a0253f1fa4276cf67e1fb39
manuel <manuelturlan14@gmail.com>
2022-07-03 14:59:01 -0400

    Ejercicio 9 de javascript terminado

---
+++ fechas.js
@@ -0,0 +1,15 @@
+let todayDate = new Date();
+
+let myDateOfBirth = new Date('June 9, 2001')
+myDateOfBirth
+
+let itsLater = todayDate > myDateOfBirth
+
+itsLater
+
+let bornDay = myDateOfBirth.getDay();
+bornDay
+
+let month = myDateOfBirth.getMonth();
+
+let year = myDateOfBirth.getFullYear();
